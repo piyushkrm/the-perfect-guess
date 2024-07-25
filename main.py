6585289d6d5a4398dc7c36bf9f46712891a2796c
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-25 17:27:20 +0530

    project is run successfully

---
+++ main.py
@@ -0,0 +1,29 @@
+'''
+We are going to write a program that generates a random number and asks the user to
+guess it.
+
+If the player's guess is higher than the actual number, the program displays "Lower
+number please". Similarly, if the user's guess is too low, the program prints "higher
+number please" When the user guesses the correct number, the program displays the
+number of guesses the player used to arrive at the number.
+
+Hint: Use the random module.
+'''
+
+import random
+
+computer = random.randint(1, 100)
+user = -1
+
+guesses = 0
+
+while user != computer :
+    guesses += 1
+
+    user = int(input("Guess the number : "))
+
+    if user > computer:
+        print("Please lower number enter !")
+    else : 
+        print("Please enter higher number !")
+print(f"You have guessed the number {computer} correctly in {guesses} attempts")
